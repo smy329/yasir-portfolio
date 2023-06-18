6597166f9af162b39b31367a43b4ec17c6902b9d
Syed Mohammad Yasir <smy329@gmail.com>
2023-06-18 20:52:43 +0600

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,8 +13,8 @@ const Navbar = () => {
       className={`${styles.paddingX} w-full flex items-center py-2 fixed 
       top-0 z-20 bg-flashWhite sm:opacity-[0.97] xxs:h-[12vh]`}
     >
-      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
-        <Link
+      <div className="w-full flex justify-end items-center max-w-7xl mx-auto">
+        {/* <Link
           to="/"
           className="flex items-center gap-2"
           onClick={() => {
@@ -28,22 +28,20 @@ const Navbar = () => {
             className="sm:w-[50px] sm:h-[50px] w-[45px] h-[45px] object-contain"
           />
 
-          {/* if you have text you want besides your logo it comes here.
-          Otherwise delete this if you don't need it. */}
           <img
             src={logotext}
             alt="logo"
             className="sm:w-[90px] sm:h-[90px] w-[85px] h-[85px] -ml-[0.6rem] object-contain"
           />
-        </Link>
-        <ul className="list-none hidden sm:flex flex-row gap-14 mt-2">
+        </Link> */}
+        <ul className="list-none hidden sm:flex flex-row gap-8 mt-2">
           {navLinks.map((nav) => (
             <li
               key={nav.id}
               className={`${
                 active === nav.title ? 'text-french' : 'text-eerieBlack'
               } hover:text-taupe text-[21px] font-medium font-mova 
-                uppercase tracking-[3px] cursor-pointer nav-links`}
+                uppercase tracking-[2px] cursor-pointer nav-links`}
               onClick={() => setActive(nav.title)}
             >
               <a href={`#${nav.id}`}>{nav.title}</a>
